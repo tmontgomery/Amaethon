506f49bee9d4566816470f77c82b0146574ca2fb
Todd L. Montgomery <tmont@nard.net>
2015-05-06 18:03:21 -0700

    exercise runner

---
+++ solutions/basic-service/src/main/java/amaethon/RunExercise.java
@@ -0,0 +1,8 @@
+package amaethon;
+
+public class RunExercise
+{
+    // TODO: create AuctionService (which is where exercise is)
+    // TODO: create AutomatedClient to submit new auctions, bids, etc.
+    // TODO: verify AuctionHouse has correct values, etc.
+}
