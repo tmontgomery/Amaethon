200bd5cba010eefe61a4922decb97847a777db73
Todd L. Montgomery <tmont@nard.net>
2015-05-08 11:48:50 -0700

    added advanced-service exercise module and all run tasks now have stubs.

---
+++ activity-feed/src/main/java/amaethon/RunExercise.java
@@ -0,0 +1,15 @@
+package amaethon;
+
+/**
+ * Runner for the exercise that has a contained Media Driver, AuctionService and AutomatedClient
+ */
+public class RunExercise
+{
+    public static final String SUBMISSION_CHANNEL = "aeron:udp?remote=localhost:43456";
+    public static final int SUBMISSION_STREAM_ID = 1;
+
+    public static void main(final String[] args) throws Exception
+    {
+        System.out.print("***\n*** Exercise incomplete\n***\n");
+    }
+}
